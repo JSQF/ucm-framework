b288f0488a87030299f7cadefe1f3475f4ec3e8c
huawei <woshihuawei@hotmail.com>
2016-01-10 22:11:32 +0800

    1. 修改项目查询列表sql 2. 修改项目列表操作css

--- ucm-web-projects/ucm-web/src/main/webapp/assets/js/project-manager-datatable.js
+++ ucm-web-projects/ucm-web/src/main/webapp/assets/js/project-manager-datatable.js
@@ -1,6 +1,16 @@
 var ProjectManagerDataTable = {
     projectManagerDataTable : null,
 
+    //---------------------------
+    // 更新项目
+    //---------------------------
+    showUpdateProjectBtn: null,
+    updateProjectModal: null,
+    closeUpdateProjectModalBtn: null,
+    checkUpdateProjectBtn : null,
+
+
+
     reload : function(){
         this.projectManagerDataTable.fnDraw();
     },
@@ -36,12 +46,40 @@ var ProjectManagerDataTable = {
             }, {
                 "title": "操作",
                 "render": function (data, type, row) {
-                    var updateBtn = '<a href="javascript:;" class="updateProject" data_id="' + row.id + '">修改</a>';
-                    var viewVersionBtn = '<a href="javascript:;" class="showProjectVersionConig" data_id="' + row.id + '">查看配置版本</a>';
+                    var updateBtn = '<a href="javascript:;" class="btn purple btn-xs showUpdateProjectBtn" data_id="' + row.id + '">修改</a>';
+                    var viewVersionBtn = '<a href="javascript:;" class="btn purple btn-xs showProjectVersionConig" data_id="' + row.id + '">查看配置版本</a>';
                     return updateBtn + "&nbsp;" + viewVersionBtn;
                 }
-            }]
+            }],
+            "lengthMenu": [ 1, 25, 50, 75, 100 ],
+            "oLanguage": {
+                "sLengthMenu": "每页显示 _MENU_条",
+                "sZeroRecords": "没有找到符合条件的数据",
+                "sProcessing": "<img src='/assets/wait.gif' />",
+                "sInfo": "当前第 _START_ - _END_ 条　共计 _TOTAL_ 条",
+                "sInfoEmpty": "木有记录",
+                "sInfoFiltered": "(从 _MAX_ 条记录中过滤)",
+                "sSearch": "搜索：",
+                "oPaginate": {
+                    "sFirst": "首页",
+                    "sPrevious": "前一页",
+                    "sNext": "后一页",
+                    "sLast": "尾页"
+                }
+            }
         });
+
+        this.projectManagerDataTable.on('draw.dt', function(){
+            if(ProjectManagerDataTable.showUpdateProjectBtn != null) {
+                ProjectManagerDataTable.showUpdateProjectBtn.off('click');
+            }
+
+            ProjectManagerDataTable.showUpdateProjectBtn = $('.showUpdateProjectBtn');
+            ProjectManagerDataTable.showUpdateProjectBtn.on('click', function(){
+
+            })
+        })
+
 
         $(".updateProject").on("click", function(){
             $.ajax({
